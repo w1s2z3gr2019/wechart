068c7a3a5dd7466f407388a96031f10e4b5d98b8
liting <29177211@qq.com>
2019-06-13 23:14:38 +0800

    update

--- pages/my/my.js
+++ pages/my/my.js
@@ -16,19 +16,43 @@ Component({
    */
   data: {
     name: '',
-    imgUrl: '../../image/tt.jpg'
+    imgUrl: '../../image/tt.jpg',
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   attached() {
-    console.log(app)
-    this.setData({
-      name: app.globalData.userInfo.nickName,
-      imgUrl: app.globalData.userInfo.avatarUrl
-    })
+    if (app.globalData.userInfo) {
+      this.setData({
+        name: app.globalData.userInfo.nickName,
+        imgUrl: app.globalData.userInfo.avatarUrl
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          name: app.globalData.userInfo.nickName,
+          imgUrl: app.globalData.userInfo.avatarUrl
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          console.log(res)
+          this.setData({
+            name: app.globalData.userInfo.nickName,
+            imgUrl: app.globalData.userInfo.avatarUrl
+          })
+        }
+      })
+    }
   },
   methods: {
+
   }
 })
